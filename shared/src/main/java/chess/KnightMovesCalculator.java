82664797ef1519b785fe72c5dd1159d74720535a
Noah Dunn <ndunn5@byu.edu>
2025-01-20 16:12:02 -0700

    woohoo everything but pawn

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -5,6 +5,16 @@ import java.util.Collection;
 public class KnightMovesCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        int[][] directions = {
+                {1, 2},
+                {2, 1},
+                {-1, 2},
+                {-2, 1},
+                {1, -2},
+                {2, -1},
+                {-1, -2},
+                {-2, -1}
+        };
+        return ChessHelper.SingleMoves(board, myPosition, directions);
     }
 }
