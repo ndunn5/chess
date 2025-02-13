92bb4696227078cdc76423eace36258a0ecabd0e
Noah Dunn <ndunn5@byu.edu>
2025-02-12 20:44:42 -0700

    added GameData.java

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,37 @@
+package model;
+import chess.ChessGame;
+
+public class GameData {
+    private final int gameID;
+    private final String whiteUsername;
+    private final String blackUsername;
+    private final String gameName;
+    private ChessGame game;
+    GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game){
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
+
+    public int getGameID(){
+        return gameID;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public String getBlackUsername(){
+        return blackUsername;
+    }
+
+    public String getGameName(){
+        return gameName;
+    }
+
+    public ChessGame getGame(){
+        return game;
+    }
+}
