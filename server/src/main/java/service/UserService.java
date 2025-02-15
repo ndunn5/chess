ab06ae500fc3a865db55f2f6dfd1687d2cd00b93
Noah Dunn <ndunn5@byu.edu>
2025-02-14 17:07:18 -0700

    made an interface

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -4,7 +4,8 @@ import model.RegisterRequest;
 import model.LogoutRequest;
 
 public class UserService {
-    public RegisterResult register(RegisterRequest registerRequest) {}
-    public LoginResult login(LoginRequest loginRequest) {}
-    public void logout(LogoutRequest logoutRequest) {}
+//    public RegisterResult register(RegisterRequest registerRequest) {}
+//    public LoginResult login(LoginRequest loginRequest) {}
+//    public void logout(LogoutRequest logoutRequest) {}
+    //if you have an error you throw something other than the registerRequest, you want to throw the error
 }
