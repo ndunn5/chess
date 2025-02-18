d691a1e2336ab64f8fed7a2b501b166b6c84d9bd
Noah Dunn <ndunn5@byu.edu>
2025-02-18 06:32:35 -0700

    made AuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,17 @@
 package dataaccess;
 
+import model.AuthData;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuthDAO {
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+
+    public void insertAuth(AuthData auth) {
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    public AuthData getAuth(String authToken) {
+        return authTokens.get(authToken);
+    }
 }
