e702947c9e496f862e9c2d35e1bcc11732446f1a
Noah Dunn <ndunn5@byu.edu>
2025-02-18 06:53:36 -0700

    put in the GameDAO

---
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -0,0 +1,36 @@
+package dataaccess;
+
+import model.GameData;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameDAO {
+    private int nextGameID = 1;
+    private Map<Integer, GameData> allGames = new HashMap<>();
+
+    public int insertGame(GameData game){
+        int thisGameID = nextGameID;
+        allGames.put(thisGameID, new GameData(thisGameID, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game()));
+        nextGameID ++;
+        return thisGameID;
+    }
+
+    public GameData getGame(int gameID){
+        return allGames.get(gameID);
+    }
+
+    public void deleteGame(int gameID){
+        allGames.remove(gameID);
+    }
+
+    public void updateGame(GameData game){
+        if (allGames.containsKey(game.gameID())){
+            allGames.put(game.gameID(), game);
+        }
+    }
+
+    public Map<Integer, GameData> returnAllGames(){
+        return allGames;
+    }
+
+}
