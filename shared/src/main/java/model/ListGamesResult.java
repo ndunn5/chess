fa9564e248679ae859cafa8523b76d7a351af6c7
Noah Dunn <ndunn5@byu.edu>
2025-02-28 01:35:44 -0700

    formatted the listgames

--- shared/src/main/java/model/ListGamesResult.java
+++ shared/src/main/java/model/ListGamesResult.java
@@ -1,11 +1,12 @@
 package model;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public record ListGamesResult (Map<Integer, GameData> allGames, String message) {
-    public ListGamesResult(Map<Integer, GameData> allGames){
-        this(allGames, null);
+public record ListGamesResult (List<Map<String, Object>> games, String message) {
+    public ListGamesResult(List<Map<String, Object>> games){
+        this(games, null);
     }
     public ListGamesResult(String message){
         this(null, message);
