c837c519a53ece048a6456993fb21d002643cae5
Noah Dunn <ndunn5@byu.edu>
2025-03-22 23:07:02 -0600

    okay the board is coming along

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import chess.*;
+import exception.ResponseException;
 import ui.Repl;
 import server.ServerFacade;
 
@@ -8,6 +9,13 @@ public class Main {
         if (args.length == 1) {
             serverUrl = args[0];
         }
+        try {
+            ServerFacade serverFacade = new ServerFacade(serverUrl);
+            serverFacade.handleClearDatabase();
+        } catch (ResponseException ex){
+            System.out.println("An error occurred while clearing the database: " + ex.getMessage());
+        }
+
         new Repl(serverUrl).run();
     }
 }
--- client/src/main/java/ui/GamePlay.java
+++ client/src/main/java/ui/GamePlay.java
@@ -12,6 +12,7 @@ import model.LogoutRequest;
 import model.LogoutResult;
 import server.ServerFacade;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +21,9 @@ public class GamePlay {
 
     private final ServerFacade server;
     private final String serverUrl;
+    private final ArrayList<String> sideLetters = new ArrayList<>(Arrays.asList(" ", "a", "b", "c", "d", "e", "f", "g", "h", " "));
+    private final ArrayList<String> sideNumbers = new ArrayList<>(Arrays.asList(" ", "1", "2", "3", "4", "5", "6", "7", "8", " "));
+
 
     public GamePlay(String serverUrl) {
         server = new ServerFacade(serverUrl);
@@ -40,20 +44,45 @@ public class GamePlay {
         }
     }
 
-    public String showBoard(String boardState, String whiteOrBlack){
+    public String showBoard(String boardState, String whiteOrBlack) {
 //        "This is where the board will be printed. It needs to take in an object in and also white orientation or black orientation";
         Gson gson = new Gson();
 
         JsonObject jsonObject = JsonParser.parseString(boardState).getAsJsonObject();
         JsonObject boardObject = jsonObject.getAsJsonObject("board");
         ChessBoard board = gson.fromJson(boardObject, ChessBoard.class);
-
-        ChessPiece testPiece = board.getPiece(new ChessPosition(1,1));
-        System.out.println(testPiece);
+        if (whiteOrBlack.equals("WHITE")) {
+            return drawWhiteBoard(board, whiteOrBlack);
+        }
 
         return boardState + whiteOrBlack;
     }
 
+    public String drawWhiteBoard(ChessBoard board, String whiteOrBlack) {
+        StringBuilder boardDisplay = new StringBuilder();
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 0 || i == 9) {
+                boardDisplay.append(printHeaderAndFooter(whiteOrBlack) + "\n");
+            }
+        }
+
+        return boardDisplay.toString();
+    }
+
+    private StringBuilder printHeaderAndFooter(String color) {
+        StringBuilder headerAndFooterBuilder = new StringBuilder();
+        headerAndFooterBuilder.append(EscapeSequences.SET_BG_COLOR_DARK_GREY);
+
+        for (String letter : sideLetters) {
+            headerAndFooterBuilder.append(EscapeSequences.SET_TEXT_COLOR_WHITE + letter + "   ");
+        }
+
+        headerAndFooterBuilder.append(EscapeSequences.RESET_BG_COLOR);
+        headerAndFooterBuilder.append(EscapeSequences.RESET_TEXT_COLOR);
+
+        return headerAndFooterBuilder;
+    }
 
 
     public String help() {
