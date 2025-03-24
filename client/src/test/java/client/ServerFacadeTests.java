d378c7d0a3b9d7acfe379775ac97c9cd9f110fce
Noah Dunn <ndunn5@byu.edu>
2025-03-24 07:04:21 -0600

    finished unit tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,11 +2,13 @@ package client;
 
 import dataaccess.UserDAO;
 import exception.ResponseException;
-import model.LoginRequest;
-import model.RegisterRequest;
+import extramodel.JoinGameRequest;
+import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
 import server.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ServerFacadeTests {
 
@@ -45,8 +47,136 @@ public class ServerFacadeTests {
         } catch (ResponseException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    @Test
+    @DisplayName("Register Positive")
+    public void registerPositive() {
+        assertDoesNotThrow(() -> {
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+        });
+    }
 
+    @Test
+    @DisplayName("Register Negative")
+    public void registerNegative() {
+        try{
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            facade.handleLogout(new LogoutRequest(registerResult.authToken()));
+            Assertions.assertThrows(ResponseException.class, () -> facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail")));
+        }catch(ResponseException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Login Positive")
+    public void loginPositive() {
+        try{
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            facade.handleLogout(new LogoutRequest(registerResult.authToken()));
+            assertDoesNotThrow(() -> {
+                LoginResult loginResult = facade.handleLogin(new LoginRequest("testUserName", "testPassword"));
+            });
+        } catch (ResponseException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Login Negative")
+    public void loginNegative() {
+            Assertions.assertThrows(ResponseException.class, () -> facade.handleLogin(new LoginRequest("testUserName", "testPassword")));
+    }
+
+    @Test
+    @DisplayName("Logout Positive")
+    public void logoutPositive() {
+        try{
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            assertDoesNotThrow(() -> {
+                facade.handleLogout(new LogoutRequest(registerResult.authToken()));
+            });
+        } catch (ResponseException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Logout Negative")
+    public void logoutNegative() {
+        Assertions.assertThrows(ResponseException.class, () -> facade.handleLogout(new LogoutRequest("fakeAuthToken")));
+    }
+
+    @Test
+    @DisplayName("List Games Positive")
+    public void listGamesPositive() {
+        try{
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            assertDoesNotThrow(() -> {
+                facade.handleListGames(new ListGamesRequest(registerResult.authToken()));
+            });
+        } catch (ResponseException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("List Games Negative")
+    public void listGamesNegative() {
+        Assertions.assertThrows(ResponseException.class, () -> facade.handleListGames(new ListGamesRequest("fakeAuthToken")));
+    }
+
+    @Test
+    @DisplayName("Join Game Positive")
+    public void joinGamePositive() {
+        try{
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            CreateGameResult createGameResult = facade.handleCreateGame(new CreateGameRequest(registerResult.authToken(), "testGame"));
+            assertDoesNotThrow(() -> {
+                JoinGameRequest joinGameRequest = new JoinGameRequest("WHITE", createGameResult.gameID());
+                joinGameRequest.addAuthToken(registerResult.authToken());
+                facade.handleJoinGame(joinGameRequest);
+            });
+        } catch (ResponseException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Join Game Negative")
+    public void joinGameNegative() {
+        try{
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            CreateGameResult createGameResult = facade.handleCreateGame(new CreateGameRequest(registerResult.authToken(), "testGame"));
+            Assertions.assertThrows(ResponseException.class, () -> facade.handleJoinGame(new JoinGameRequest("white", createGameResult.gameID())));
+        } catch (ResponseException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Create Game Positive")
+    public void createGamePositive() {
+        try{
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            assertDoesNotThrow(() -> {
+                CreateGameResult createGameResult = facade.handleCreateGame(new CreateGameRequest(registerResult.authToken(), "testGame"));
+            });
+        } catch (ResponseException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Create Game Negative")
+    public void createGameNegative() {
+        try{
+            RegisterResult registerResult = facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            Assertions.assertThrows(ResponseException.class, () -> facade.handleCreateGame(new CreateGameRequest("fakeAuthToken", "testGame")));
+        } catch (ResponseException e){
+            throw new RuntimeException(e);
+        }
     }
 
 }
