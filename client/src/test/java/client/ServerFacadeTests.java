67a0f5442516fae93779d81627edcff9e16f6a3b
Noah Dunn <ndunn5@byu.edu>
2025-03-24 06:25:29 -0600

    one unit test down

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,32 @@
 package client;
 
+import dataaccess.UserDAO;
+import exception.ResponseException;
+import model.LoginRequest;
+import model.RegisterRequest;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
 
-//serverfacade->repl -> prelogin client and then the rest
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
+    }
+    @BeforeEach
+    public void setup(){
+        try{
+            facade.handleClearDatabase();
+        } catch (ResponseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @AfterAll
@@ -22,8 +36,17 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @DisplayName("Clear Database")
+    public void clearDatabase() {
+        try{
+            facade.handleRegister(new RegisterRequest("testUserName", "testPassword", "testEmail"));
+            facade.handleClearDatabase();
+            Assertions.assertThrows(ResponseException.class, () -> facade.handleLogin(new LoginRequest("testUserName", "testPassword")));
+        } catch (ResponseException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 }
